a797332b53b607cdc5b001413a43641054035498
Elisabeth Gross <elisabethgross568@gmail.com>
2016-06-24 09:41:32 -0400

    initial

---
+++ models/database.js
@@ -0,0 +1,5 @@
+'use strict';
+let config = require('../config.json');
+let Sequelize = require('sequelize');
+
+module.exports = new Sequelize(config.database, config.username, config.password, config);
